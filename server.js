9c010d0ab7aff5ef56c399cdea00971b76a4c924
Siddhi Goel <siddhigoel93@gmail.com>
2026-06-19 23:20:49 +0530

    configured mongodb setup

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
-const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const connectDB = require("./src/config/db");
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-  console.log(`🌍 Environment: ${process.env.NODE_ENV}`);
+const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("ERP Backend Running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
